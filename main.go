d3188af9ada1154b796876406c655a31ab2e0c44
VaroWz <antoine.bouille.24@gmail.com>
2022-06-12 00:17:33 +0200

    Update main.go
    Update checkWin

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-var one = []string{"?", "?", "?", "?", "?", "?", "?", "?", "?"}
+var one = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
 var choose int
 var isWin bool
 
@@ -40,11 +40,38 @@ func board() {
 
 func checkWin() {
 
-	if one[0] == one[1] && one[0] == one[2] || one[0] == one[3] && one[0] == one[6] || one[6] == one[7] && one[6] == one[8] || one[2] == one[5] && one[2] == one[8] {
+	if one[0] == one[1] && one[0] == one[2] {
 		if one[0] == "X" {
 			fmt.Println("Vous avez gagné !!!")
 		}
 		if one[0] == "O" {
+			fmt.Println("Vous avez perdu !")
+		}
+		isWin = true
+	}
+	if one[0] == one[3] && one[0] == one[6] {
+		if one[0] == "X" {
+			fmt.Println("Vous avez gagné !!!")
+		}
+		if one[0] == "O" {
+			fmt.Println("Vous avez perdu !")
+		}
+		isWin = true
+	}
+	if one[6] == one[7] && one[6] == one[8] {
+		if one[6] == "X" {
+			fmt.Println("Vous avez gagné !!!")
+		}
+		if one[6] == "O" {
+			fmt.Println("Vous avez perdu !")
+		}
+		isWin = true
+	}
+	if one[2] == one[5] && one[2] == one[8] {
+		if one[2] == "X" {
+			fmt.Println("Vous avez gagné !!!")
+		}
+		if one[2] == "O" {
 			fmt.Println("Vous avez perdu !")
 		}
 		isWin = true
