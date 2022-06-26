c128fa541b5fd2e1df4043123b7773ae69a3a145
VaroWz <antoine.bouille.24@gmail.com>
2022-06-27 00:29:36 +0200

    Update automaticPlay.go
    Fix bugs

--- automaticPlay.go
+++ automaticPlay.go
@@ -5,108 +5,84 @@ import "math/rand"
 func automaticPlay() {
 
 	//horizontale haut
-	if placeLoc(1) == "X" && placeLoc(2) == "X" {
-		if placeLoc(3) != "X" {
-			one[2] = "O"
-		}
-	} else if placeLoc(1) == "X" && placeLoc(3) == "X" {
-		if placeLoc(2) != "X" {
+	if placeLoc(1) == "X" && placeLoc(2) == "X" && placeLoc(3) != "O" {
+		one[2] = "O"
+
+	} else if placeLoc(1) == "X" && placeLoc(3) == "X" && placeLoc(2) != "O" {
 			one[1] = "O"
-		}
-	} else if placeLoc(2) == "X" && placeLoc(3) == "X" {
-		if placeLoc(1) != "X" {
+		
+	} else if placeLoc(2) == "X" && placeLoc(3) == "X" && placeLoc(1) != "O" {
 			one[0] = "O"
-		}
+		
 		//horizontale centre
-	} else if placeLoc(4) == "X" && placeLoc(5) == "X" {
-		if placeLoc(6) != "X" {
+	} else if placeLoc(4) == "X" && placeLoc(5) == "X" && placeLoc(6) != "O" {
 			one[5] = "O"
-		}
-	} else if placeLoc(5) == "X" && placeLoc(6) == "X" {
-		if placeLoc(4) != "X" {
+		
+	} else if placeLoc(5) == "X" && placeLoc(6) == "X" && placeLoc(4) != "O" {
 			one[3] = "O"
-		}
 		//horizontale bas
-	} else if placeLoc(7) == "X" && placeLoc(8) == "X" {
-		if placeLoc(9) != "X" {
+	} else if placeLoc(7) == "X" && placeLoc(8) == "X" && placeLoc(9) != "O" {
 			one[8] = "O"
-		}
-	} else if placeLoc(7) == "X" && placeLoc(9) == "X" {
-		if placeLoc(8) != "X" {
+		
+	} else if placeLoc(7) == "X" && placeLoc(9) == "X" && placeLoc(8) != "O" {
 			one[7] = "O"
-		}
-	} else if placeLoc(8) == "X" && placeLoc(9) == "X" {
-		if placeLoc(7) != "X" {
+		
+	} else if placeLoc(8) == "X" && placeLoc(9) == "X" && placeLoc(7) != "O" {
 			one[6] = "O"
-		}
+		
 		//verticale gauche
-	} else if placeLoc(1) == "X" && placeLoc(4) == "X" {
-		if placeLoc(7) != "X" {
+	} else if placeLoc(1) == "X" && placeLoc(4) == "X" && placeLoc(7) != "O" {
 			one[6] = "O"
-		}
-	} else if placeLoc(1) == "X" && placeLoc(7) == "X" {
-		if placeLoc(4) != "X" {
+		
+	} else if placeLoc(1) == "X" && placeLoc(7) == "X" && placeLoc(4) != "O" {
 			one[3] = "O"
-		}
-	} else if placeLoc(4) == "X" && placeLoc(7) == "X" {
-		if placeLoc(1) != "X" {
+		
+	} else if placeLoc(4) == "X" && placeLoc(7) == "X" && placeLoc(1) != "O" {
 			one[0] = "O"
-		}
+		
 		//verticale centre
-	} else if placeLoc(2) == "X" && placeLoc(5) == "X" {
-		if placeLoc(8) != "X" {
+	} else if placeLoc(2) == "X" && placeLoc(5) == "X" && placeLoc(8) != "O" {
 			one[7] = "O"
-		}
-	} else if placeLoc(2) == "X" && placeLoc(8) == "X" {
-		if placeLoc(5) != "X" {
+		
+	} else if placeLoc(2) == "X" && placeLoc(8) == "X" && placeLoc(5) != "O" {
 			one[4] = "O"
-		}
-	} else if placeLoc(5) == "X" && placeLoc(8) == "X" {
-		if placeLoc(2) != "X" {
+		
+	} else if placeLoc(5) == "X" && placeLoc(8) == "X" && placeLoc(2) != "O" {
 			one[1] = "O"
-		}
+		
 		//verticale droit
-	} else if placeLoc(3) == "X" && placeLoc(6) == "X" {
-		if placeLoc(9) != "X" {
+	} else if placeLoc(3) == "X" && placeLoc(6) == "X" && placeLoc(9) != "O" {
 			one[8] = "O"
-		}
-	} else if placeLoc(3) == "X" && placeLoc(9) == "X" {
-		if placeLoc(6) != "X" {
+		
+	} else if placeLoc(3) == "X" && placeLoc(9) == "X" && placeLoc(6) != "O" {
 			one[5] = "O"
-		}
-	} else if placeLoc(6) == "X" && placeLoc(9) == "X" {
-		if placeLoc(3) != "X" {
+		
+	} else if placeLoc(6) == "X" && placeLoc(9) == "X" && placeLoc(3) != "O" {
 			one[2] = "O"
-		}
+		
 		//diago /
-	} else if placeLoc(3) == "X" && placeLoc(5) == "X" {
-		if placeLoc(7) != "X" {
+	} else if placeLoc(3) == "X" && placeLoc(5) == "X" && placeLoc(7) != "O" {
 			one[6] = "O"
-		}
-	} else if placeLoc(3) == "X" && placeLoc(7) == "X" {
-		if placeLoc(5) != "X" {
+		
+	} else if placeLoc(3) == "X" && placeLoc(7) == "X" && placeLoc(5) != "O" {
 			one[4] = "O"
-		}
-	} else if placeLoc(5) == "X" && placeLoc(7) == "X" {
-		if placeLoc(3) != "X" {
+		
+	} else if placeLoc(5) == "X" && placeLoc(7) == "X" && placeLoc(3) != "O" {
 			one[2] = "O"
-		}
+		
 		//diago \
-	} else if placeLoc(1) == "X" && placeLoc(5) == "X" {
-		if placeLoc(9) != "X" {
+	} else if placeLoc(1) == "X" && placeLoc(5) == "X" && placeLoc(9) != "O" {
 			one[8] = "O"
-		}
-	} else if placeLoc(1) == "X" && placeLoc(9) == "X" {
-		if placeLoc(5) != "X" {
+		
+	} else if placeLoc(1) == "X" && placeLoc(9) == "X" && placeLoc(5) != "O" {
 			one[4] = "O"
-		}
-	} else if placeLoc(5) == "X" && placeLoc(9) == "X" {
-		if placeLoc(1) != "X" {
+		
+	} else if placeLoc(5) == "X" && placeLoc(9) == "X" && placeLoc(1) != "O" {
 			one[0] = "O"
-		}
+		
 	} else {
 		a := rand.Intn(9)
-		if placeLoc(a-1) != "X" {
+		if placeLoc(a-1) != "O" {
 			one[a] = "O"
 		}
 	}
